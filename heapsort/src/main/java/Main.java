2b51f94f4eed49338405692a2e545bb331e1f62d
Bayusov Alexander <62991332+diosquierra@users.noreply.github.com>
2020-09-20 23:53:11 +0700

    Update Main.java
    Some changes of tests.

--- heapsort/src/main/java/Main.java
+++ heapsort/src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
         int[] test3 = {100, 14, 10, -100, 5};
         int[] test4 = {100, 100, 50, 60, 20};
         int[] test5 = {6, 3, 2, - 10, -10};
+        int[] test6 = {59, 60, 49, -19, 1, 100, -1000};
 
         // This is the same arrays but sorted
         int[] test1s = {1, 2, 3, 4, 5, 6};
@@ -17,6 +18,7 @@ public class Main {
         int[] test3s = {-100, 5, 10, 14, 100};
         int[] test4s = {20, 50, 60, 100, 100};
         int[] test5s = {-10, -10, 2, 3, 6};
+        int[] test6s = {-1000, -19, 1, 49, 59, 60, 100};
 
         HeapSort sorting = new HeapSort();
         sorting.sort(test1);
@@ -24,12 +26,14 @@ public class Main {
         sorting.sort(test3);
         sorting.sort(test4);
         sorting.sort(test5);
+        sorting.sort(test6);
 
         assertArrayEquals(test1, test1s);
         assertArrayEquals(test2, test2s);
         assertArrayEquals(test3, test3s);
         assertArrayEquals(test4, test4s);
         assertArrayEquals(test5, test5s);
+        assertArrayEquals(test6, test6s);
 
     }
 }
