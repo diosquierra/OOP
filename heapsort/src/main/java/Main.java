355926adbb7f51e5a71adf0644d11e62f7cfbe06
Bayusov Alexander <62991332+diosquierra@users.noreply.github.com>
2020-09-24 17:32:16 +0700

    Add files via upload

--- heapsort/src/main/java/Main.java
+++ heapsort/src/main/java/Main.java
@@ -1,39 +1,14 @@
-import static org.junit.Assert.assertArrayEquals;
+import nsu.oop.heapsort.HeapSort;
 
 public class Main {
     public static void main(String[] args){
+        int[] arrayOne = {5, 4, 3, 2, 1};
+        HeapSort abc = new HeapSort();
+        abc.sort(arrayOne);
 
-        // In main we are testing our sorting with this tests
-        // This is arrays which we should sort
-        int[] test1 = {1, 2, 3, 4, 5, 6};
-        int[] test2 = {6, 5, 4, 3, 2, 1};
-        int[] test3 = {100, 14, 10, -100, 5};
-        int[] test4 = {100, 100, 50, 60, 20};
-        int[] test5 = {6, 3, 2, - 10, -10};
-        int[] test6 = {59, 60, 49, -19, 1, 100, -1000};
-
-        // This is the same arrays but sorted
-        int[] test1s = {1, 2, 3, 4, 5, 6};
-        int[] test2s = {1, 2, 3, 4, 5, 6};
-        int[] test3s = {-100, 5, 10, 14, 100};
-        int[] test4s = {20, 50, 60, 100, 100};
-        int[] test5s = {-10, -10, 2, 3, 6};
-        int[] test6s = {-1000, -19, 1, 49, 59, 60, 100};
-
-        HeapSort sorting = new HeapSort();
-        sorting.sort(test1);
-        sorting.sort(test2);
-        sorting.sort(test3);
-        sorting.sort(test4);
-        sorting.sort(test5);
-        sorting.sort(test6);
-
-        assertArrayEquals(test1, test1s);
-        assertArrayEquals(test2, test2s);
-        assertArrayEquals(test3, test3s);
-        assertArrayEquals(test4, test4s);
-        assertArrayEquals(test5, test5s);
-        assertArrayEquals(test6, test6s);
+        for (int num : arrayOne) {
+            System.out.print(num + ", ");
+        }
 
     }
 }
---
+++ heapsort/src/test/java/JUnitTestClass.java
@@ -0,0 +1,39 @@
+import nsu.oop.heapsort.HeapSort;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class JUnitTestClass {
+    @Test
+    void HeapSortTest(){
+    // This is arrays which we should sort
+    int[] test1 = {1, 2, 3, 4, 5, 6};
+    int[] test2 = {6, 5, 4, 3, 2, 1};
+    int[] test3 = {100, 14, 10, -100, 5};
+    int[] test4 = {100, 100, 50, 60, 20};
+    int[] test5 = {6, 3, 2, - 10, -10};
+    int[] test6 = {59, 60, 49, -19, 1, 100, -1000};
+
+    // This is the same arrays but sorted
+    int[] test1s = {1, 2, 3, 4, 5, 6};
+    int[] test2s = {1, 2, 3, 4, 5, 6};
+    int[] test3s = {-100, 5, 10, 14, 100};
+    int[] test4s = {20, 50, 60, 100, 100};
+    int[] test5s = {-10, -10, 2, 3, 6};
+    int[] test6s = {-1000, -19, 1, 49, 59, 60, 100};
+
+    HeapSort sorting = new HeapSort();
+        sorting.sort(test1);
+        sorting.sort(test2);
+        sorting.sort(test3);
+        sorting.sort(test4);
+        sorting.sort(test5);
+        sorting.sort(test6);
+
+    assertArrayEquals(test1, test1s);
+    assertArrayEquals(test2, test2s);
+    assertArrayEquals(test3, test3s);
+    assertArrayEquals(test4, test4s);
+    assertArrayEquals(test5, test5s);
+    assertArrayEquals(test6, test6s);
+    }
+}
